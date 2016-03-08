2a0c9080fcf0ed79237ba312ef94d280e231dd34
jfambro <Jonathan.Fambro@careerbuilder.com>
2016-03-08 12:47:17 -0500

    Changes made

--- exercise5.rb
+++ exercise5.rb
@@ -16,3 +16,33 @@ puts "His teeth are usually #{my_teeth} depending on the coffee"
 #this line is tricky, try to get it exactly right
 puts "If I add #{my_age}, #{my_height}, add #{my_weight} I get #{my_age + my_height + my_weight}."
 
+#Exercise
+#1.Changing the variable name
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee"
+
+#this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, add #{weight} I get #{age + height + weight}."
+
+#2.Create variables and convert inches to centimeter & pounds to kilograms
+centimeter = height * 2.54
+kilogram =  weight / 2.2
+
+puts "His height in centimeters is #{centimeter}"
+puts "His weight in kilograms is #{kilogram}"
+
+
+
+
